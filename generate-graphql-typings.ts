de0a3d0fd01646c66e4f1f3fdd128520637c449d
Hector Salazar <hecsalazarf@outlook.com>
2019-07-18 17:02:06 -0500

    Script to generate types from schema

---
+++ generate-graphql-typings.ts
@@ -0,0 +1,15 @@
+import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
+import { join } from 'path';
+
+/*
+* Script that generates type definitions from GraphQL schema.
+* To run: npx ts-node generate-graphql-typings
+*/
+
+const definitionsFactory = new GraphQLDefinitionsFactory();
+definitionsFactory.generate({
+  typePaths: ['./src/graphql/prisma/*.graphql'],
+  path: join(process.cwd(), 'src/graphql/graphql.schema.d.ts'),
+  outputAs: 'class',
+  watch: true,
+});

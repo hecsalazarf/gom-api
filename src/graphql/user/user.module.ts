4dbe95d0cd0fdd7ac920dfd9d9de758dc0559660
Hector Salazar <hecsalazarf@outlook.com>
2019-07-18 17:01:10 -0500

    Removed unused files

--- src/graphql/user/user.module.ts
+++
@@ -1,9 +0,0 @@
-
-import { Module } from '@nestjs/common';
-import { UserResolver } from './user.resolver';
-// import { RecipesService } from './recipes.service';
-
-@Module({
-  providers: [UserResolver],
-})
-export class UserModule {}

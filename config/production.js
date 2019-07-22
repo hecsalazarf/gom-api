0517344563af7a31e9bab7cc5bc47c9350358820
Hector Salazar <hecsalazarf@outlook.com>
2019-07-22 11:01:14 -0500

    Break before user code starts

---
+++ config/production.js
@@ -0,0 +1,13 @@
+module.exports = {
+  keys: ['hola', 'mundo'],
+  auth0: {
+    url: 'https://arkio.auth0.com/',
+    issuer: 'https://arkio.auth0.com/',
+    grantType: 'password',
+    clientId: 'tk37Qxx0ozSe3k2W9THkVnuj35eU7FDo',
+    clientSecret: '43TAIqHtRt40d2-d8ZltJW_0_YJ4WURo5zgTFS_dthPqZpPvCPZJ9GA8FI35dCZx',
+    audience: 'http://dev.api.gom',
+    scope: 'openid',
+    jwksEndpoint: 'https://arkio.auth0.com/.well-known/jwks.json',
+  }
+}

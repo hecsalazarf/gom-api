8b1e686e7c337450088f8da7be68c2c669c353ae
Hector Salazar <hecsalazarf@outlook.com>
2019-08-03 10:37:50 -0500

    chore: redis on destroy logger

--- src/db/redis/hooks/ondestroy.hook.ts
+++ src/db/redis/hooks/ondestroy.hook.ts
@@ -1,12 +1,15 @@
-import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, Logger } from '@nestjs/common';
 import { RedisService } from '../redis.service';
 
 @Injectable()
 export class OnRedisDestroy implements OnModuleDestroy {
+  private readonly logger: Logger = new Logger(OnRedisDestroy.name);
+
   constructor(private readonly redis: RedisService) {}
 
   // app.enableShutdownHooks() must have been called to enable this hook
   async onModuleDestroy() {
     await this.redis.quit(); // disconnect redis gracefully
+    this.logger.log('Redis gracefully disconnected');
   }
 }

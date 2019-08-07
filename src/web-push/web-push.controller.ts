6763227ee1e413180d1d8058148dd41c70f6587f
Hector Salazar <hecsalazarf@outlook.com>
2019-08-06 20:53:42 -0500

    chore: catch errors in controller

--- src/web-push/web-push.controller.ts
+++ src/web-push/web-push.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Request, HttpCode, HttpStatus, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, Request, HttpCode, HttpStatus, ValidationPipe, InternalServerErrorException } from '@nestjs/common';
 import { WebPushService } from './web-push.service';
 import { SubscriptionDto } from './dto';
 
@@ -9,12 +9,20 @@ export class WebPushController {
   @Post('subscribe')
   @HttpCode(HttpStatus.CREATED)
   async subscribe(@Body(new ValidationPipe()) subs: SubscriptionDto, @Request() req) {
-    return await this.webpush.addUserSubscription(req.user.id, subs);
+    try {
+      return await this.webpush.addUserSubscription(req.user.id, subs);
+    } catch (error) {
+      throw new InternalServerErrorException('Subscription could not be stored', 'webpush_subscribe');
+    }
   }
 
   @Post('unsubscribe')
   @HttpCode(HttpStatus.CREATED)
   async unsubscribe(@Body(new ValidationPipe()) subs: SubscriptionDto, @Request() req) {
-    return await this.webpush.removeUserSubscription(req.user.id, subs);
+    try {
+      return await this.webpush.removeUserSubscriptions(req.user.id, [subs.endpoint]);
+    } catch (error) {
+      throw new InternalServerErrorException('Subscription could not be stored', 'webpush_subscribe');
+    }
   }
 }

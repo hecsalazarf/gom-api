8952d0649c7d599f02b87deff04729910a06c485
Hector Salazar <hecsalazarf@outlook.com>
2019-08-21 22:28:18 -0500

    chore: remove csrf nest middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,4 @@
 export * from './cookie.middleware';
-export * from './csrf.middleware';
 export * from './csrfgen.middleware';
 export * from './session.middleware';
 export * from './auth.middleware';

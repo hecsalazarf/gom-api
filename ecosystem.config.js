d46ae25a312f14d8677f848a97f72af147a5dac0
Hector Salazar <hecsalazarf@outlook.com>
2019-08-26 21:10:11 -0500

    feat: add pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps : [{
+    name: 'gom-api',
+    script: './dist/main.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    restart_delay: 3000,
+    watch: false,
+    max_memory_restart: '500M',
+    source_map_support: true,
+    autorestart: true,
+    restart_delay: 3000,
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_staging: {
+      NODE_ENV: 'staging'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};

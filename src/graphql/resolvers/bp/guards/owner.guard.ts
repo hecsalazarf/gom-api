e4731204b59a0d3be5dcde4b5779ad00cd488fea
Hector Salazar <hecsalazarf@outlook.com>
2019-08-29 20:32:39 -0500

    feat: resource only allowed to owner

--- src/graphql/resolvers/bp/guards/owner.guard.ts
+++ src/graphql/resolvers/bp/guards/owner.guard.ts
@@ -3,7 +3,9 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { Reflector } from '@nestjs/core';
 import { ForbiddenError, ApolloError } from 'apollo-server-errors';
 import { PrismaService } from '../../../../db/prisma/prisma.service';
-import { BpWhereInput } from '../../../../db/prisma/prisma.binding';
+import { BpWhereInput, BpWhereUniqueInput, BpSubscriptionWhereInput, BpCreateInput } from '../../../graphql.schema';
+
+import { Ability } from '@casl/ability';
 
 @Injectable()
 export class BpOwnerGuard implements CanActivate {
@@ -13,35 +15,82 @@ export class BpOwnerGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const gqlCtx: any = GqlExecutionContext.create(context);
+    const gqlCtx = GqlExecutionContext.create(context);
+    // @ts-ignore // omit handler no-type compilation error
     const permission: string[] = this.reflector.get('permission', gqlCtx.handler); // get metadata from handler (resolver)
-    if (typeof permission === 'undefined' || !permission.includes('own')) {
-      // if no permission whatsoever, or own permission, allow execution by default
+    if (typeof permission === 'undefined') {
+      // if no permission whatsoever, allow execution by default
       return true;
     }
-    const { where } = gqlCtx.getArgs(); // get args
+    const args = gqlCtx.getArgs(); // get args
     const { user } = gqlCtx.getContext(); // get user
-    const filter: BpWhereInput = {};
-    if (user.ability.can('role', 'seller')) {
-      // if customer, found the order issued to him
-      filter.customerOf_some = {
-        extUid: user.id,
-      };
-    } else {
-      // no role found, error
-      this.logger.error('No role found to validate BP owner');
-      throw new ApolloError('Cannot validate BP owner owner at this moment', 'SERVER_ERROR');
-    }
+
     let res: boolean;
-    try {
-      res = await this.prisma.cexists.Bp({ ...where, ...filter });
-    } catch (error) {
-      this.logger.error(error.message);
+    if (permission.includes('read:bp') || permission.includes('update:bp') || permission.includes('delete:bp')) {
+      res = await this.checkWhereUniqueInput(args.where, user);
+    } else if (permission.includes('read:bps')) {
+      res = this.checkWhereInput(args.where || '', user);
+    } else if (permission.includes('create:bp')) {
+      res = this.checkDataInput(args.data, user);
+    } else if (permission.includes('subscribe:bp')) {
+      res = this.checkSubscriptionWhereInput(args.where, user);
+    } else {
+      // no permission whatsoever, allow execution by default
+      return true;
+    }
+
+    if (typeof res === 'undefined') {
       throw new ApolloError('Cannot validate BP owner at this moment', 'SERVER_ERROR');
     }
     if (!res) {
       throw new ForbiddenError('Cannot access this BP');
     }
     return true;
+  }
+
+  private async checkWhereUniqueInput(where: BpWhereUniqueInput, user: { id: string, ability: Ability}): Promise<boolean | undefined> {
+    const filter: BpWhereInput = {};
+    if (user.ability.can('role', 'seller')) {
+      // if customer, found the BP issued to him
+      filter.customerOf_some = {
+        extUid: user.id,
+      };
+    } else {
+      // no role found, error
+      return undefined;
+    }
+    try {
+      return await this.prisma.cexists.Bp({ ...where, ...filter });
+    } catch (error) {
+      this.logger.error(error.message);
+      return undefined;
+    }
+  }
+
+  private checkWhereInput(where: BpWhereInput, user: { id: string, ability: Ability}): boolean | undefined {
+    if (user.ability.can('role', 'seller') && where.customerOf_some) {
+      // if seller, found the BP assigned to him
+      return where.customerOf_some.extUid === user.id;
+    } else {
+      return undefined;
+    }
+  }
+
+  private checkDataInput(data: BpCreateInput, user: { id: string, ability: Ability}): boolean | undefined {
+    if (user.ability.can('role', 'seller') && data.customerOf && data.customerOf.connect) {
+      // if seller, found the BP assigned to him
+      return data.customerOf.connect.findIndex(u => u.extUid === user.id) !== -1;
+    } else {
+      return undefined;
+    }
+  }
+
+  private checkSubscriptionWhereInput(where: BpSubscriptionWhereInput, user: { id: string, ability: Ability}): boolean | undefined {
+    if (user.ability.can('role', 'seller') && where.node && where.node.customerOf_some) {
+      // if seller, found the BP assigned to him
+      return where.node.customerOf_some.extUid === user.id;
+    } else {
+      return undefined;
+    }
   }
 }
--- src/graphql/resolvers/order/guards/owner.guard.ts
+++ src/graphql/resolvers/order/guards/owner.guard.ts
@@ -3,7 +3,8 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { Reflector } from '@nestjs/core';
 import { ForbiddenError, ApolloError } from 'apollo-server-errors';
 import { PrismaService } from '../../../../db/prisma/prisma.service';
-import { OrderWhereInput } from '../../../../db/prisma/prisma.binding';
+import { OrderWhereInput, OrderWhereUniqueInput, OrderSubscriptionWhereInput, OrderCreateInput } from '../../../graphql.schema';
+import { Ability } from '@casl/ability';
 
 @Injectable()
 export class OrderOwnerGuard implements CanActivate {
@@ -13,14 +14,40 @@ export class OrderOwnerGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const gqlCtx: any = GqlExecutionContext.create(context); // any type to omit handler no-type compilation error
+    const gqlCtx = GqlExecutionContext.create(context);
+    // @ts-ignore // omit handler no-type compilation error
     const permission: string[] = this.reflector.get('permission', gqlCtx.handler); // get metadata from handler (resolver)
-    if (typeof permission === 'undefined' || !permission.includes('own')) {
-      // if no permission whatsoever, or own permission, allow execution by default
+    if (typeof permission === 'undefined') {
+      // if no permission whatsoever, allow execution by default
       return true;
     }
-    const { where } = gqlCtx.getArgs(); // get args
+    const args = gqlCtx.getArgs(); // get args
     const { user } = gqlCtx.getContext(); // get user
+
+    let res: boolean;
+    if (permission.includes('read:order') || permission.includes('update:order') || permission.includes('delete:order')) {
+      res = await this.checkWhereUniqueInput(args.where, user);
+    } else if (permission.includes('read:orders')) {
+      res = this.checkWhereInput(args.where || '', user);
+    } else if (permission.includes('create:order')) {
+      res = this.checkDataInput(args.data, user);
+    } else if (permission.includes('subscribe:order')) {
+      res = this.checkSubscriptionWhereInput(args.where, user);
+    } else {
+      // no permission whatsoever, allow execution by default
+      return true;
+    }
+
+    if (typeof res === 'undefined') {
+      throw new ApolloError('Cannot validate order owner at this moment', 'SERVER_ERROR');
+    }
+    if (!res) {
+      throw new ForbiddenError('Cannot access this order');
+    }
+    return true;
+  }
+
+  private async checkWhereUniqueInput(where: OrderWhereUniqueInput, user: { id: string, ability: Ability}): Promise<boolean | undefined> {
     const filter: OrderWhereInput = {};
     if (user.ability.can('role', 'customer')) {
       // if customer, found the order issued to him
@@ -34,19 +61,46 @@ export class OrderOwnerGuard implements CanActivate {
       };
     } else {
       // no role found, error
-      this.logger.error('No role found to validate order owner');
-      throw new ApolloError('Cannot validate order owner at this moment', 'SERVER_ERROR');
+      return undefined;
     }
-    let res: boolean;
     try {
-      res = await this.prisma.cexists.Order({ ...where, ...filter });
+      return await this.prisma.cexists.Order({ ...where, ...filter });
     } catch (error) {
       this.logger.error(error.message);
-      throw new ApolloError('Cannot validate order owner at this moment', 'SERVER_ERROR');
+      return undefined;
     }
-    if (!res) {
-      throw new ForbiddenError('Cannot access this order');
+  }
+
+  private checkWhereInput(where: OrderWhereInput, user: { id: string, ability: Ability}): boolean | undefined {
+    if (user.ability.can('role', 'customer') && where.issuedTo) {
+      return where.issuedTo.uid === user.id;
+    } else if (user.ability.can('role', 'seller') && where.assignedTo) {
+      // if seller, found the order assigned to him
+      return where.assignedTo.extUid === user.id;
+    } else {
+      return undefined;
     }
-    return true;
+  }
+
+  private checkDataInput(data: OrderCreateInput, user: { id: string, ability: Ability}): boolean | undefined {
+    if (user.ability.can('role', 'customer') && data.issuedTo) {
+      return data.issuedTo.connect.uid === user.id;
+    } else if (user.ability.can('role', 'seller') && data.assignedTo) {
+      // if seller, found the order assigned to him
+      return data.assignedTo.connect.extUid === user.id;
+    } else {
+      return undefined;
+    }
+  }
+
+  private checkSubscriptionWhereInput(where: OrderSubscriptionWhereInput, user: { id: string, ability: Ability}): boolean | undefined {
+    if (user.ability.can('role', 'customer') && where.node && where.node.issuedTo) {
+      return where.node.issuedTo.uid === user.id;
+    } else if (user.ability.can('role', 'seller') && where.node && where.node.assignedTo) {
+      // if seller, found the order assigned to him
+      return where.node.assignedTo.extUid === user.id;
+    } else {
+      return undefined;
+    }
   }
 }

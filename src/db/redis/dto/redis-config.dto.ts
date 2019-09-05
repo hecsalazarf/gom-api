affbfcd11934831625f72ff35f28d0ee594631db
Hector Salazar <hecsalazarf@outlook.com>
2019-09-05 12:39:41 -0500

    fix: convert string to number in order to accept values from env variables

--- src/db/redis/dto/redis-config.dto.ts
+++ src/db/redis/dto/redis-config.dto.ts
@@ -1,12 +1,16 @@
-import { IsString, IsPositive, Max } from 'class-validator';
+import { IsString, IsPositive, Max, IsInt } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class RedisConfigDto {
+  @IsInt()
   @IsPositive()
+  @Type(() => Number) // convert string to number
   readonly port: number;
 
   @IsString()
   readonly host: string;
 
+  @IsInt()
   @IsPositive()
   @Max(15)
   readonly db: number;
